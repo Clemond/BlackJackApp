d6db3ede3642998674821759cdd38768d1e7f870
Clemond <quackshows11@gmail.com>
2024-10-04 14:24:32 +0200

    Initial Commit

---
+++ BlackJackApp/BlackJackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackAppApp.swift
+//  BlackJackApp
+//
+//  Created by Nicholas Nieminen Jönsson on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
